e3ceb13e47c024d60f72e9b939548f385e6f5a99
אורית אופיר <orit902@gmail.com>
2021-11-02 17:29:07 +0200

    ok

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function register(email, password) {
+  const response = await fetch('http://localhost:4000/register', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password })
+  })
+  if (response.ok) {
+    const { token } = await response.json();
+    localStorage.setItem("token", token)
+    return
+  }
+
+  return response.text()
 }
 
+// function myAlert (text){
+//   alert(text)
+// }
+
+async function login(email, password) {
+  const response = await fetch('http://localhost:4000/login', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password })
+  })
+  const { token } = await response.json();
+  localStorage.setItem("token", token)
+  console.log("logged in successfuly!!")
+}
+
+
+
+function App() {
+
+  const textRef = useRef();
+  const passRef = useRef();
+  const [stateText, setStateText] = useState("");
+  const [loudOrNot, setLoudOrNot] = useState('')
+
+  async function add_me() {
+    setLoudOrNot("louding...")
+    setLoudOrNot(await register(textRef.current.value, passRef.current.value))
+    
+  }
+
+  async function log_in() {
+    setLoudOrNot("louding...")
+    setLoudOrNot(await login(textRef.current.value, passRef.current.value))
+    
+  }
+
+  /* return (
+     <div className= "App">
+       <h1>welcome!</h1>
+       <input type="text" value={stateText} onChange={()=>myAlert(stateText)} name="text1" />
+       <input type="password" name="password1"/>
+       <input type="button" name="addMe" value="add me" onClick= {() => { alert(textRef.current.value);}} />
+     </div>
+   );*/
+
+  return (
+    <div className="App">
+      <h1>welcome!</h1>
+      <h5>Enter your email:</h5>
+      <input ref={textRef} type="text" />
+      <h5>Enter your password:</h5>
+      <input ref={passRef} type="password" />
+      <div>
+        <input value="add me" type="Submit" onClick={add_me} />
+      </div>
+      <div>
+        <input value="I have user" type="Submit" onClick={log_in} />
+      </div>
+      <div>
+        <span value={loudOrNot}>{loudOrNot}</span>
+      </div>
+    </div>
+  );
+
+}
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const jwt = require('jsonwebtoken')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 
 /*
 mongoose.connect(process.env.DB_CONNECTION, (error)=>{
@@ -13,11 +17,11 @@ mongoose.connect(process.env.DB_CONNECTION, (error)=>{
     }
 */
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 mongoose.connect(process.env.DB_CONNECTION)
-    .then(()=>{
+    .then(() => {
         console.log("Mongo connection success!!")
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log("Mango connection error: ", error)
     })
 
@@ -39,29 +43,64 @@ const User = mongoose.model('User', userSchema)
     res.send("t")
 })*/
 
-app.post('/register',async (req,res)=> {
-try{
-    const {email, password } = req.query
-    const existingUser = await User.findOne({email}).exec()
-    if (existingUser){
-        res.status(400).send("User Already Exists")
-        return;
+app.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        console.log("in")
+        console.log(email)
+        console.log(password)
+
+        if (email == null || password == null) {
+            console.log("error")
+            res.status(400).send("Missing data")
+            return res;
+        }
+        if (password.length < 8 || email.length < 6 || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+            console.log("error")
+            res.status(400).send("Missing data")
+            return res;
+        }
+
+        const existingUser = await User.findOne({ email }).exec()
+        console.log({existingUser})
+        if (existingUser) {
+            res.status(400).send("User Already Exists")
+            console.log("00000000000000000000000000000000")
+            return res;
+        }
+        console.log("ok!!!")
+        res.json({ token: jwt.sign({ email }, process.env.SECRET, { expiresIn: "2h" }) })
+        const newUser = await User.create({ email, password })
+        return res;
     }
-    const newUser= await User.create({email, password})
-    res.status(201).send("User creat")
-}
-catch(error){
+
+catch (error) {
     console.log("Error: ", error)
+    res.status(500).send(error)
 }
 })
 
+app.post('/login', async (req, res) => {
+    console.log("login", req.body)
+    try {
+        const { email, password } = req.body
+        const existingUser = await User.findOne({ email, password }).exec()
+        if (!existingUser) {
+            res.status(400).send("User or Password Invalid")
+            return;
+        }
 
+        res.json({ token: jwt.sign({ email }, process.env.SECRET, { expiresIn: "2h" }) })
+
+    } catch (error) {
+        console.log("Error: ", error)
+        res.status(500).send(error)
+    }
+})
 
 const port = process.env.PORT
-const server = app.listen(port, ()=>{
-    console.log("start port: "+ port)
+const server = app.listen(port, () => {
+    console.log("start port: " + port)
 })
 
 /*console.log("server: ", server)*/
-
-console.log(process.env)
